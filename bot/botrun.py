ce3a7570843f0a8f8b36c3d477761d8a3245e9d2
semimaks <semimaks@mail.ru>
2021-08-08 20:20:53 +0300

    Добавляем разрешения боту

--- bot/botrun.py
+++ bot/botrun.py
@@ -1,7 +1,14 @@
 import os
 
+import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print('Ботан на старте/ Nerd on start')
+
 
 bot.run(os.getenv('TOKEN'))
