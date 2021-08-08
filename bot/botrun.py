3e2389fc1c4e09ac68e858884dbc4ffbe1dd535f
semimaks <semimaks@mail.ru>
2021-08-08 20:49:33 +0300

    git rm --f botrun.bat

--- bot/botrun.py
+++ bot/botrun.py
@@ -1,6 +1,5 @@
-import os
-
 import discord
+import os, sqlite3
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
@@ -8,7 +7,13 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print('Ботан на старте/ Nerd on start')
+    print('Ботан на старте. Nerd on start')
+
+    global base, cur
+    base = sqlite3.connect('nerd.db')
+    cur = base.cursor()
+    if base:
+        print('DataBase connected...OK')
 
 
 bot.run(os.getenv('TOKEN'))
