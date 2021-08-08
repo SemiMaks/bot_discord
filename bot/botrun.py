279cf20b9dd1e42e358b1992eba0b4c299a7da62
semimaks <semimaks@mail.ru>
2021-08-08 20:02:26 +0300

    Создаем бота, получаем токен и авторизуем его

---
+++ bot/botrun.py
@@ -0,0 +1,7 @@
+import os
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+bot.run(os.getenv('TOKEN'))
---
+++ botrun.bat
@@ -0,0 +1,11 @@
+@echo off
+
+call %~dp0\venv\Scripts\activate
+
+cd %~dp0bot\
+
+set TOKEN=ODczOTY1NjAzODEwMDc4Nzkw.YRAF5Q.ynnHD3KY9tWJ_7AuUvubeG04U6A
+
+python botrun.py
+
+pause
