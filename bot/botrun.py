0e68961253ed7022094d91abf8bd473c534ac8ee
semimaks <semimaks@mail.ru>
2021-08-08 23:02:31 +0300

    Добавляем команды для бота

--- bot/botrun.py
+++ bot/botrun.py
@@ -2,7 +2,8 @@ import discord
 import os, sqlite3
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='!')
+# bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 
 @bot.event
@@ -15,5 +16,12 @@ async def on_ready():
     if base:
         print('DataBase connected...OK')
 
+@bot.command()
+async def test(ctx):
+    await ctx.send('туточки я')
+
+@bot.command()
+async def info(ctx, *, arg):
+    await ctx.send(arg)
 
 bot.run(os.getenv('TOKEN'))
